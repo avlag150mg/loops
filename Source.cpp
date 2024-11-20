6c93ee6f103b33ebef9fea76dcdcecfe90466370
illya <korsillua@gmail.com>
2024-11-20 18:50:19 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <Windows.h>
+using namspace std;
+
+int main() {
+	int number = 0;
+	do
+	{
+		cout << "Enter a number between 0 and 10: ";
+		cin >> number;
+		if (number < 0 || number > 10)
+		{
+			cout << ""
+		}
+	}
+}

73d7c40aa525d429e9a9313c67a0def041d8160d
illya <korsillua@gmail.com>
2024-11-20 18:52:28 +0200

    loops

--- Source.cpp
+++ Source.cpp
@@ -2,17 +2,8 @@
 #include <conio.h>
 #include <iomanip>
 #include <Windows.h>
-using namspace std;
+using namespace std;
 
 int main() {
-	int number = 0;
-	do
-	{
-		cout << "Enter a number between 0 and 10: ";
-		cin >> number;
-		if (number < 0 || number > 10)
-		{
-			cout << ""
-		}
-	}
+	return 0;
 }

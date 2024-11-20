5502075bb141dbd85bc567a4e4d2c8291c50c218
illya <korsillua@gmail.com>
2024-11-20 20:48:48 +0200

    ex2

--- Source.cpp
+++ Source.cpp
@@ -5,5 +5,46 @@
 using namespace std;
 
 int main() {
-	return 0;
+    int lower, upper;
+
+    cout << "Enter the first boundary of the range: ";
+    cin >> lower;
+    cout << "Enter the second boundary of the range: ";
+    cin >> upper;
+
+    if (lower > upper) {
+        swap(lower, upper);
+    }
+
+    cout << "\nAll numbers in the range [" << lower << ", " << upper << "]:" << endl;
+    for (int i = lower; i <= upper; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << "\nEven numbers in the range:" << endl;
+    for (int i = lower; i <= upper; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    cout << "\nOdd numbers in the range:" << endl;
+    for (int i = lower; i <= upper; i++) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    cout << "\nNumbers divisible by 7 in the range:" << endl;
+    for (int i = lower; i <= upper; i++) {
+        if (i % 7 == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
 }
